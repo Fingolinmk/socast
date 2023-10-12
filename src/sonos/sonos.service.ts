fd45aa63220a208f0943ac420845c7933e77845b
Moritz Kempkes <Moritz.Kempkes@outlook.com>
2023-10-12 17:45:07 +0200

    changed the sonos lib with svrooij/sonos

--- src/sonos/sonos.service.ts
+++ src/sonos/sonos.service.ts
@@ -1,34 +1,41 @@
 import { Injectable } from '@nestjs/common';
-import { Sonos, AsyncDeviceDiscovery } from 'sonos';
+import { SonosManager, SonosDevice } from '@svrooij/sonos';
 
 @Injectable()
 export class SonosService {
-  private sonos: Sonos;
-
+  private manager: SonosManager;
+  private activeDevice: SonosDevice;
   constructor() {
-    this.sonos = new Sonos('192.168.178.53');
+    this.manager = new SonosManager();
+    this.manager.InitializeWithDiscovery(10);
   }
 
   async playPodcast(url: string): Promise<string> {
     try {
-      await this.sonos.play(url);
-
+      this.activeDevice.PlayNotification({ trackUri: url });
       return `Playing podcast from URL: ${url}`;
     } catch (error) {
       throw new Error(`Failed to play podcast: ${error.message}`);
     }
   }
 
-  async getDevices(): Promise<string[]> {
-    const discovery = new AsyncDeviceDiscovery();
-    const sonosDevices = await discovery.discoverMultiple();
-    const devices_ip: string[] = sonosDevices.map((device) => device.host);
-    return devices_ip;
+  async getDevices(): Promise<
+    { name: string; groupname: string; uuid: string }[]
+  > {
+    const sonosDevices = this.manager.Devices;
+
+    const devicesInfo: { name: string; groupname: string; uuid: string }[] =
+      sonosDevices.map((device) => ({
+        name: device.Name,
+        groupname: device.GroupName,
+        uuid: device.Uuid,
+      }));
+    return devicesInfo;
   }
-  async setActiveDevice(deviceIP: string) {
-    this.sonos = new Sonos(deviceIP);
+  async setActiveDevice(deviceID: string) {
+    this.activeDevice = this.manager.Devices.find((i) => i.Uuid === deviceID);
   }
   async getActiveDevice(): Promise<string> {
-    return this.sonos.host;
+    return this.activeDevice.Uuid;
   }
 }
